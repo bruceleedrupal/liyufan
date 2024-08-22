9c8cf259665b58775985a145fe138bb8d75a730a
liyufan <bruelee.drupal@gmail.com>
2024-08-22 21:15:11 +0800

    save

--- a.cpp
+++ a.cpp
@@ -2,23 +2,7 @@
 using namespace std;
 int main()
 {
-  int i=0;
-  int x=0;
-  int z=1;
-  while (x==0)
-  {
-    i++;
-    x+=i;
-    
+  int n;
+  cin>>n;
 
-  }
-  
 }
-
-
-/**
-* @runId: 32715
-* @language: C++
-* @author: 2024zzzx6
-* @submitTime: 2024-06-25 20:18:02
-*/
--- b.cpp
+++ b.cpp
@@ -4,39 +4,23 @@
 #include <iomanip>
 #include <algorithm>
 using namespace std;
-int fenzi(int n){
+int abc(int n){
     if(n==1||n==2){
         return 1;
     }
     else{
-        return fenzi(n-1)+fenzi(n-2);
+        return abc(n-1)+abc(n-2);
     }
 }
-
-int fenmu(int n){
-    if(n==1){
-        return 1;
-    }
-    else if(n==2){
-        return 2;
-    }
-    else{
-        return fenmu(n-1)+fenmu(n-2);
-    }
-}
-
-
 int main()
 {
     int n;
     cin>>n;
-
-    double  x=0;
+    int x=n;
     for(int i=1;i<=n;i++){
-        x+=(double)fenzi(i)/fenmu(i);
+        abc(x)=abc(x-1)+n;
     }
-    
-    cout<<fixed<<setprecision(3)<<x;
+    cout<<abc(n);
 
     return 0;
 }
