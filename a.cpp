f6128ddc56da431e28cccf7890ab657dbad0f8ab
liyufan <bruelee.drupal@gmail.com>
2024-08-23 12:59:58 +0800

    save

--- a.cpp
+++ a.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 using namespace std;
+int gcd(int a, int b)
+{
+  if (b == 0)
+  {
+    return a;
+  }
+  return gcd(b, a % b);
+}
+
 int main()
 {
-  int n;
-  cin>>n;
+  int m, n;
+  cin >> m >> n;
 
+  int x = gcd(m, n);
+
+  int a = m / x;
+  int b = n / x;
+  cout << x << endl;
+
+  return 0;
 }
--- b.cpp
+++ b.cpp
@@ -4,23 +4,14 @@
 #include <iomanip>
 #include <algorithm>
 using namespace std;
-int abc(int n){
-    if(n==1||n==2){
-        return 1;
-    }
-    else{
-        return abc(n-1)+abc(n-2);
-    }
-}
 int main()
 {
     int n;
     cin>>n;
-    int x=n;
+    int x=0;
     for(int i=1;i<=n;i++){
-        abc(x)=abc(x-1)+n;
+        x+=i*i;
     }
-    cout<<abc(n);
-
+    cout<<x;
     return 0;
 }
